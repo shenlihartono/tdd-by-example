6807447f3882a949e8432d655cfc70a0f07b550e
Stanley <shenlihartono@gmail.com>
2022-07-25 20:52:21 +0700

    TDD 9: external currency param when constructing Money object

--- src/main/java/xie/stanley/training/Dollar.java
+++ src/main/java/xie/stanley/training/Dollar.java
@@ -1,18 +1,20 @@
 package xie.stanley.training;
 
 public class Dollar extends Money {
+	private String currency;
 
-	public Dollar(int amount) {
+	public Dollar(int amount, String currency) {
 		this.amount = amount;
+		this.currency = currency;
 	}
 
 	@Override
 	public String currency() {
-		return "USD";
+		return currency;
 	}
 
 	public Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return new Dollar(amount * multiplier, "USD");
 	}
 
 }
--- src/main/java/xie/stanley/training/Franc.java
+++ src/main/java/xie/stanley/training/Franc.java
@@ -1,18 +1,20 @@
 package xie.stanley.training;
 
 public class Franc extends Money {
-
-	public Franc(int amount) {
+	private String currency;
+	
+	public Franc(int amount, String currency) {
 		this.amount = amount;
+		this.currency= currency;
 	}
 
 	@Override
 	public String currency() {
-		return "CHF";
+		return currency;
 	}
 
 	public Money times(int multiplier) {
-		return new Franc(amount * multiplier);
+		return new Franc(amount * multiplier, "CHF");
 	}
 
 }
