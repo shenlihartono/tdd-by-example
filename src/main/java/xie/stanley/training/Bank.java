3107d2d6472323f5b9c30322873a98db475a58b4
Stanley <shenlihartono@gmail.com>
2022-07-29 20:47:05 +0700

    TDD 14: Refactoring: - add "Pair" for saving pair of source & target currency into Map - Expression interface method "reduce" to add bank param - method reduce in Money class to accept bank param (to query rate)

--- src/main/java/xie/stanley/training/Bank.java
+++ src/main/java/xie/stanley/training/Bank.java
@@ -1,8 +1,24 @@
 package xie.stanley.training;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+	private Map<Pair, Integer> rates = new HashMap<>();
+	
 	Money reduce(Expression source, String toCurrency) {
-		return source.reduce(toCurrency);
+		return source.reduce(this, toCurrency);
 	}
 	
+	public int rate(String from, String to) {
+		if (from.equals(to)) {
+			return 1;
+		}
+		
+		return rates.get(new Pair(from, to));
+	}
+
+	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), rate);
+	}
 }
---
+++ src/main/java/xie/stanley/training/Pair.java
@@ -0,0 +1,29 @@
+package xie.stanley.training;
+
+import java.util.Objects;
+
+public class Pair {
+	private final String from;
+	private final String to;
+
+	public Pair(String from, String to) {
+		this.from = from;
+		this.to = to;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Pair))
+			return false;
+		
+		Pair pair = (Pair) o;
+		return from.equals(pair.from) && to.equals(pair.to);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(from, to);
+	}
+}
